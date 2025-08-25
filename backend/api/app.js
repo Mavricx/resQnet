c5c6095a14b0941a9421121785e05064ae867bcf
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-08-25 18:50:53 +0530

    backend in making

---
+++ backend/api/app.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+dotenv.config({ path: "../../.env" })
+import connectDB from "../utils/db.js";
+import express from 'express'
+
+const app = express()
+
+app.get("/help", (_, res) => {
+    res.send("sending help")
+})
+
+connectDB().then(() => {
+    // eslint-disable-next-line no-undef
+    const port = process.env.PORT || 3000
+    app.listen(port, () => {
+        console.log(`Server started on port:${port}`)
+    })
+})
+
+
