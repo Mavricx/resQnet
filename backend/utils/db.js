3c89126367320b5f2b454ada6a42137f204a6848
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-08-25 18:54:38 +0530

    backend in making

---
+++ backend/utils/db.js
@@ -0,0 +1,18 @@
+//function to connect to the mongodb db.
+import dotenv from "dotenv"
+dotenv.config({ path: "../../.env" })
+import mongoose from 'mongoose';
+const connectDB = async () => {
+    // eslint-disable-next-line no-undef
+    const dbUrl = process.env.MONGO_STRING
+    try {
+        await mongoose.connect(dbUrl);
+        console.log("MongoDB connected");
+    }
+    catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
